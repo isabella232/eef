fd02bb9421219015a2fc1eee9a96f0675cf1a6bc
Goulwen Le Fur <goulwen.lefur@obeo.fr>
2012-08-29 23:25:06 +0200

    UPDATED: Service Injection configuration

--- tests/org.eclipse.emf.eef.runtime.tests/src/org/eclipse/emf/eef/runtime/tests/cases/NonUIEditingTestCase.java
+++ tests/org.eclipse.emf.eef.runtime.tests/src/org/eclipse/emf/eef/runtime/tests/cases/NonUIEditingTestCase.java
@@ -7,60 +7,48 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcorePackage;
-import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.emf.eef.runtime.EEFRuntime;
-import org.eclipse.emf.eef.runtime.binding.AbstractPropertiesEditingProvider;
 import org.eclipse.emf.eef.runtime.binding.PropertiesEditingComponent;
 import org.eclipse.emf.eef.runtime.binding.PropertiesEditingProvider;
+import org.eclipse.emf.eef.runtime.binding.PropertiesEditingProviderImpl;
 import org.eclipse.emf.eef.runtime.context.PropertiesEditingContext;
-import org.eclipse.emf.eef.runtime.context.impl.PropertiesEditingContextFactoryImpl;
 import org.eclipse.emf.eef.runtime.editingModel.EditingModelBuilder;
 import org.eclipse.emf.eef.runtime.editingModel.PropertiesEditingModel;
+import org.eclipse.emf.eef.runtime.internal.services.viewhandler.PriorityCircularityException;
+import org.eclipse.emf.eef.runtime.services.emf.EMFServiceProvider;
 import org.eclipse.emf.eef.runtime.services.viewhandler.ViewHandler;
-import org.eclipse.emf.eef.runtime.tests.util.EEFTestStuffsBuilder;
+import org.eclipse.emf.eef.runtime.tests.util.EEFTestEnvironmentBuilder;
 import org.eclipse.emf.eef.runtime.tests.views.EClassMockView;
-import org.junit.After;
 import org.junit.Before;
 
 /**
  * @author <a href="mailto:goulwen.lefur@obeo.fr">Goulwen Le Fur</a>
  *
  */
-public class NonUIEditingTestCase {
+public class NewNonUITestCase {
+
+	protected AdapterFactory adapterFactory;
 
 	protected PropertiesEditingContext editingContext;
-	protected List<Object> views;
 	protected EObject editedObject;
-	private Collection<ViewHandler<?>> viewHandlers;
-	protected ComposedAdapterFactory adapterFactory;
-	private PropertiesEditingProvider editingProvider;
 
+	protected List<Object> views;
+	private Collection<ViewHandler<?>> viewHandlers;
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
 		editedObject = createEditedObject();
-		final PropertiesEditingModel editingModel = buildEditingModel();
-		adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
-		editingProvider = new AbstractPropertiesEditingProvider() {
-
-			/**
-			 * {@inheritDoc}
-			 * @see org.eclipse.emf.eef.runtime.binding.AbstractPropertiesEditingProvider#initSpecificEditingModel()
-			 */
-			protected Collection<? extends PropertiesEditingModel> initSpecificEditingModel() {
-				List<PropertiesEditingModel> result = new ArrayList<PropertiesEditingModel>();
-				result.add(editingModel);
-				return result;
-			}
-			
-		};
-		EEFRuntime.getPlugin().getBundle().getBundleContext().registerService(PropertiesEditingProvider.class.getName(), editingProvider, null);
-		editingContext =  createEditingContext();
+		adapterFactory = buildAdapterFactory();
+		PropertiesEditingProvider editingProvider = createEditingProvider(buildEditingModel());
+		EMFServiceProvider emfServiceProvider = createEMFServiceProvider();
+		editingContext =  createEditingContext(editingProvider, emfServiceProvider);
 		viewHandlers = editingContext.getEditingComponent().createViewHandlers();
 		views = new ArrayList<Object>();
 		for (ViewHandler<?> viewHandler : viewHandlers) {
@@ -71,15 +59,48 @@ public class NonUIEditingTestCase {
 		}
 	}
 
+
+	protected PropertiesEditingProvider createEditingProvider(final PropertiesEditingModel editingModel) {
+		PropertiesEditingProvider editingProvider = new PropertiesEditingProviderImpl() {
+
+			/**
+			 * {@inheritDoc}
+			 * @see org.eclipse.emf.eef.runtime.binding.PropertiesEditingProviderImpl#getEditingModel()
+			 */
+			@Override
+			protected PropertiesEditingModel getEditingModel() {
+				return editingModel;
+			}
+			
+			
+		};
+		//TODO: Ugly fix this after Model Changes Notification system redesign.
+		editingProvider.setBundleContext(EEFRuntime.getPlugin().getBundle().getBundleContext());
+		return editingProvider;
+	}
+
+
+	protected EMFServiceProvider createEMFServiceProvider() {
+		return new EEFTestEnvironmentBuilder().buildEMFServiceProvider();
+	}
+
+
+	protected PropertiesEditingContext createEditingContext(PropertiesEditingProvider editingProvider, EMFServiceProvider emfServiceProvider) throws PriorityCircularityException {
+		return new EEFTestEnvironmentBuilder().buildEditingContext(adapterFactory, editingProvider, emfServiceProvider, editedObject);
+	}
+
+
+	protected AdapterFactory buildAdapterFactory() {
+		return new EEFTestEnvironmentBuilder().buildAdapterFactory();
+	}
+
+	
 	protected EObject createEditedObject() {
-		EPackage ecoreModel = new EEFTestStuffsBuilder().buildEcoreSampleModel();
+		EPackage ecoreModel = new EEFTestEnvironmentBuilder().buildEcoreSampleModel();
 		return ecoreModel.getEClassifiers().get(0);
 	}
-
-	protected PropertiesEditingContext createEditingContext() {
-		return new PropertiesEditingContextFactoryImpl().createPropertiesEditingContext(adapterFactory, editedObject);
-	}
-
+	
+	
 	/**
 	 * @return
 	 */
@@ -89,17 +110,4 @@ public class NonUIEditingTestCase {
 								.withView(EClassMockView.class)
 							.build();
 	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-		for (ViewHandler<?> handler : viewHandlers) {
-			handler.dispose();
-		}
-		editingContext.dispose();
-	}
-
-
 }
