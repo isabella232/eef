bf9cebda88738f2d1fa2af3815340b7978633e8c
Goulwen Le Fur <goulwen.lefur@obeo.fr>
2012-10-27 10:51:41 +0200

    ADDED: the UndefinedPropertyEditor, creating an error label when a PropertyEditor cannot be handle during view building.

---
+++ plugins/org.eclipse.emf.eef.runtime.ui/src/org/eclipse/emf/eef/runtime/ui/view/propertyeditors/impl/undefined/editor/UndefinedPropertyEditor.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.eclipse.emf.eef.runtime.ui.view.propertyeditors.impl.undefined.editor;
+
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.eef.runtime.ui.internal.services.propertyeditors.util.EEFControlWrapperViewer;
+import org.eclipse.emf.eef.runtime.ui.view.propertyeditors.PropertyEditor;
+import org.eclipse.emf.eef.runtime.ui.view.propertyeditors.PropertyEditorViewer;
+import org.eclipse.swt.widgets.Label;
+
+/**
+ * @author <a href="mailto:goulwen.lefur@obeo.fr">Goulwen Le Fur</a>
+ *
+ */
+public class UndefinedPropertyEditor implements PropertyEditor {
+
+	private PropertyEditorViewer<EEFControlWrapperViewer<Label>> propertyEditorViewer;
+	
+	/**
+	 * @param propertyEditorViewer
+	 */
+	public UndefinedPropertyEditor(PropertyEditorViewer<EEFControlWrapperViewer<Label>> propertyEditorViewer) {
+		this.propertyEditorViewer = propertyEditorViewer;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see org.eclipse.emf.eef.runtime.ui.view.propertyeditors.PropertyEditor#init(org.eclipse.emf.ecore.EStructuralFeature)
+	 */
+	public void init(EStructuralFeature feature) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see org.eclipse.emf.eef.runtime.ui.view.propertyeditors.PropertyEditor#getPropertyEditorViewer()
+	 */
+	public PropertyEditorViewer<?> getPropertyEditorViewer() {
+		return propertyEditorViewer;
+	}
+
+}
---
+++ plugins/org.eclipse.emf.eef.runtime.ui/src/org/eclipse/emf/eef/runtime/ui/view/propertyeditors/impl/undefined/editor/UndefinedPropertyEditorProvider.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package org.eclipse.emf.eef.runtime.ui.view.propertyeditors.impl.undefined.editor;
+
+import org.eclipse.emf.eef.runtime.internal.services.DefaultService;
+import org.eclipse.emf.eef.runtime.ui.UIConstants;
+import org.eclipse.emf.eef.runtime.ui.view.propertyeditors.PropertyEditor;
+import org.eclipse.emf.eef.runtime.ui.view.propertyeditors.impl.WidgetPropertyEditorProvider;
+import org.eclipse.emf.eef.views.ElementEditor;
+import org.eclipse.emf.eef.views.toolkits.ToolkitsFactory;
+import org.eclipse.emf.eef.views.toolkits.Widget;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+
+/**
+ * @author <a href="mailto:goulwen.lefur@obeo.fr">Goulwen Le Fur</a>
+ *
+ */
+public class UndefinedPropertyEditorProvider implements WidgetPropertyEditorProvider, DefaultService {
+
+	private static final String UNDEFINED_EDITOR_NAME = "Undefined";
+	private static final Widget widget = ToolkitsFactory.eINSTANCE.createWidget();
+
+	static {
+		widget.setName(UNDEFINED_EDITOR_NAME);		
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see org.eclipse.emf.eef.runtime.ui.view.propertyeditors.ModelPropertyEditorProvider#getModel()
+	 */
+	public Widget getModel() {
+		return widget;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see org.eclipse.emf.eef.runtime.ui.view.propertyeditors.impl.WidgetPropertyEditorProvider#serviceFor(org.eclipse.emf.eef.runtime.ui.services.propertyeditors.PropertyEditorProvider.PropertyEditorContext)
+	 */
+	public boolean serviceFor(PropertyEditorContext editorContext) {
+		return true;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see org.eclipse.emf.eef.runtime.ui.services.propertyeditors.PropertyEditorProvider#getPropertyEditor(org.eclipse.emf.eef.runtime.ui.services.propertyeditors.PropertyEditorProvider.PropertyEditorContext)
+	 */
+	public PropertyEditor getPropertyEditor(PropertyEditorContext editorContext) {
+		FormToolkit toolkit = editorContext.view.getEditingComponent().getEditingContext().getOptions().getOption(UIConstants.FORM_TOOLKIT);
+		if (toolkit != null) {
+			return new UndefinedPropertyEditor(new UndefinedFormPropertyEditor((ElementEditor) editorContext.viewElement));			 
+		} else {
+			return new UndefinedPropertyEditor(new UndefinedSWTPropertyEditor((ElementEditor) editorContext.viewElement));			 
+		}
+	}
+
+}
