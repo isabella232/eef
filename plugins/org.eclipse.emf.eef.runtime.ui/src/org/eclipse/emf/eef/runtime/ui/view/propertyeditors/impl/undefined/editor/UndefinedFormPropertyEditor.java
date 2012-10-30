1d6954e43b10ea21e2143606a9254dca057141ca
Goulwen Le Fur <goulwen.lefur@obeo.fr>
2012-10-30 08:50:11 +0100

    UPDATED: Imports organized

--- plugins/org.eclipse.emf.eef.runtime.ui/src/org/eclipse/emf/eef/runtime/ui/view/propertyeditors/impl/undefined/editor/UndefinedFormPropertyEditor.java
+++ plugins/org.eclipse.emf.eef.runtime.ui/src/org/eclipse/emf/eef/runtime/ui/view/propertyeditors/impl/undefined/editor/UndefinedFormPropertyEditor.java
@@ -6,7 +6,6 @@ package org.eclipse.emf.eef.runtime.ui.view.propertyeditors.impl.undefined.edito
 import org.eclipse.emf.eef.runtime.ui.internal.services.propertyeditors.util.EEFControlWrapperViewer;
 import org.eclipse.emf.eef.runtime.ui.view.propertyeditors.FormPropertyEditor;
 import org.eclipse.emf.eef.views.ViewElement;
-import org.eclipse.emf.eef.views.toolkits.Toolkit;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
