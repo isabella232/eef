1b02e8fe10916da5ec106dbfd155777feaa5bcad
Goulwen Le Fur <goulwen.lefur@obeo.fr>
2013-03-14 17:04:47 +0100

    UPDATED: improved layout

--- plugins/org.eclipse.emf.eef.runtime.ui.fx/src/org/eclipse/emf/eef/runtime/ui/fx/view/propertyeditors/impl/fxtoolkit/text/TextFXPropertyEditor.java
+++ plugins/org.eclipse.emf.eef.runtime.ui.fx/src/org/eclipse/emf/eef/runtime/ui/fx/view/propertyeditors/impl/fxtoolkit/text/TextFXPropertyEditor.java
@@ -3,12 +3,18 @@
  */
 package org.eclipse.emf.eef.runtime.ui.fx.view.propertyeditors.impl.fxtoolkit.text;
 
+import java.util.List;
+
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.eef.runtime.ui.fx.services.view.FXViewService;
 import org.eclipse.emf.eef.runtime.ui.fx.view.propertyeditors.FXPropertyEditorViewer;
 import org.eclipse.emf.eef.runtime.ui.services.view.ViewService;
@@ -40,7 +46,7 @@ public class TextFXPropertyEditor implements FXPropertyEditorViewer<TextField> {
 	 * @see org.eclipse.emf.eef.runtime.ui.fx.view.propertyeditors.FXPropertyEditorViewer#build(javafx.scene.layout.Pane)
 	 */
 	public void build(Pane parent) {
-		//TODO: trouver l'index ligne.
+		int index = ((List<EObject>) elementEditor.eContainer().eGet(elementEditor.eContainingFeature())).indexOf(elementEditor);
 		ViewService viewService = view.getViewService();
 		FXViewService swtViewService = null;
 		if (viewService instanceof FXViewService) {
@@ -48,10 +54,10 @@ public class TextFXPropertyEditor implements FXPropertyEditorViewer<TextField> {
 		}
 		if (swtViewService != null) {
 			Label label = swtViewService.createLabel(parent, elementEditor, elementEditor.getName());
-			GridPane.setColumnIndex(label, 1);
+			GridPane.setConstraints(label, 1, index+1, 1, 1, HPos.RIGHT, VPos.CENTER);
 		}
 		text = new TextField();
-		GridPane.setColumnIndex(text, 2);
+		GridPane.setConstraints(text, 2, index+1, 1, 1, HPos.CENTER, VPos.CENTER, Priority.ALWAYS, Priority.NEVER);
 		parent.getChildren().add(text);
 		if (swtViewService != null) {
 			Control button = swtViewService.createHelpButton(parent, elementEditor);
