195f2599cf11cca8fa087bca102f64390683809a
Goulwen Le Fur <goulwen.lefur@obeo.fr>
2013-08-27 07:50:55 +0200

    FIXED: the EComboFormPropertyEditor is now a FilterablePropertyEditor (it could have CCE before this commit)

--- plugins/org.eclipse.emf.eef.runtime.ui.swt.e3/src/org/eclipse/emf/eef/runtime/ui/swt/e3/internal/view/propertyeditors/impl/emfpropertiestoolkit/ecomboeditor/EComboFormPropertyEditor.java
+++ plugins/org.eclipse.emf.eef.runtime.ui.swt.e3/src/org/eclipse/emf/eef/runtime/ui/swt/e3/internal/view/propertyeditors/impl/emfpropertiestoolkit/ecomboeditor/EComboFormPropertyEditor.java
@@ -3,14 +3,18 @@
  */
 package org.eclipse.emf.eef.runtime.ui.swt.e3.internal.view.propertyeditors.impl.emfpropertiestoolkit.ecomboeditor;
 
+import java.util.Collection;
+
 import org.eclipse.emf.eef.runtime.context.PropertiesEditingContext;
 import org.eclipse.emf.eef.runtime.ui.swt.e3.internal.view.propertyeditors.impl.StandardFormPropertyEditor;
 import org.eclipse.emf.eef.runtime.ui.swt.e3.internal.widgets.FormSingleLinePropertyViewer;
 import org.eclipse.emf.eef.runtime.ui.swt.internal.widgets.SingleLinePropertyViewer;
 import org.eclipse.emf.eef.runtime.ui.swt.resources.ImageManager;
+import org.eclipse.emf.eef.runtime.ui.swt.view.propertyeditors.FilterablePropertyEditor;
 import org.eclipse.emf.eef.runtime.ui.swt.viewer.EditUIProvidersFactory;
 import org.eclipse.emf.eef.runtime.ui.view.PropertiesEditingView;
 import org.eclipse.emf.eef.views.ElementEditor;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
@@ -20,12 +24,14 @@ import org.eclipse.ui.forms.widgets.FormToolkit;
  * @author <a href="mailto:goulwen.lefur@obeo.fr">Goulwen Le Fur</a>
  *
  */
-public class EComboFormPropertyEditor extends StandardFormPropertyEditor<SingleLinePropertyViewer> {
+public class EComboFormPropertyEditor extends StandardFormPropertyEditor<SingleLinePropertyViewer> implements FilterablePropertyEditor {
 
 	private EditUIProvidersFactory editUIProvidersFactory;
 	private ImageManager imageManager;
 	
 	private FormSingleLinePropertyViewer eComboEditor;
+
+	private Collection<ViewerFilter> filters;
 
 	/**
 	 * @param editUIProvidersFactory
@@ -60,6 +66,30 @@ public class EComboFormPropertyEditor extends StandardFormPropertyEditor<SingleL
 		eComboEditor.createContents();
 		GridData layoutData = new GridData(GridData.FILL_HORIZONTAL);
 		eComboEditor.getControl().setLayoutData(layoutData);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see org.eclipse.emf.eef.runtime.ui.swt.view.propertyeditors.FilterablePropertyEditor#addFilter(org.eclipse.jface.viewers.ViewerFilter)
+	 */
+	public void addFilter(ViewerFilter filter) {
+		filters.add(filter);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see org.eclipse.emf.eef.runtime.ui.swt.view.propertyeditors.FilterablePropertyEditor#removeFilter(org.eclipse.jface.viewers.ViewerFilter)
+	 */
+	public void removeFilter(ViewerFilter filter) {
+		filters.remove(filter);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see org.eclipse.emf.eef.runtime.ui.swt.view.propertyeditors.FilterablePropertyEditor#getFilters()
+	 */
+	public Collection<ViewerFilter> getFilters() {
+		return filters;
 	}
 
 	/**
