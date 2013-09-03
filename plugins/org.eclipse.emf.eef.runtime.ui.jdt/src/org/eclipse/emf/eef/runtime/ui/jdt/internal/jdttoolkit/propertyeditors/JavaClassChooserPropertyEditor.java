d4ae3f389b11889eb460338a748ed522d756e256
Goulwen Le Fur <goulwen.lefur@obeo.fr>
2013-09-03 14:34:12 +0200

    ADDED: JDT Toolkit

---
+++ plugins/org.eclipse.emf.eef.runtime.ui.jdt/src/org/eclipse/emf/eef/runtime/ui/jdt/internal/jdttoolkit/propertyeditors/JavaClassChooserPropertyEditor.java
@@ -0,0 +1,145 @@
+/**
+ * 
+ */
+package org.eclipse.emf.eef.runtime.ui.jdt.internal.jdttoolkit.propertyeditors;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.eef.runtime.notify.PropertiesEditingEvent;
+import org.eclipse.emf.eef.runtime.notify.PropertiesEditingEventImpl;
+import org.eclipse.emf.eef.runtime.ui.swt.internal.widgets.SingleLinePropertyViewer;
+import org.eclipse.emf.eef.runtime.ui.swt.internal.widgets.SingleLinePropertyViewer.SingleLinePropertyViewerListener;
+import org.eclipse.emf.eef.runtime.ui.swt.util.EEFViewerInput;
+import org.eclipse.emf.eef.runtime.ui.view.PropertiesEditingView;
+import org.eclipse.emf.eef.runtime.ui.view.propertyeditors.MonovaluedPropertyEditor;
+import org.eclipse.emf.eef.runtime.ui.view.propertyeditors.PropertyEditorViewer;
+import org.eclipse.emf.eef.runtime.ui.view.propertyeditors.impl.PropertyEditorImpl;
+import org.eclipse.emf.eef.views.ElementEditor;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.search.IJavaSearchConstants;
+import org.eclipse.jdt.core.search.IJavaSearchScope;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.internal.core.JavaModelManager;
+import org.eclipse.jdt.internal.ui.dialogs.FilteredTypesSelectionDialog;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * @author <a href="mailto:goulwen.lefur@obeo.fr">Goulwen Le Fur</a>
+ *
+ */
+public class JavaClassChooserPropertyEditor extends PropertyEditorImpl implements MonovaluedPropertyEditor {
+
+	protected PropertiesEditingView<Composite> view;
+	protected ElementEditor elementEditor;
+	protected PropertyEditorViewer<SingleLinePropertyViewer> propertyEditorViewer;
+	private EStructuralFeature feature;
+	private SingleLinePropertyViewerListener listener;
+
+
+	/**
+	 * @param view
+	 * @param elementEditor
+	 * @param propertyEditorViewer
+	 */
+	public JavaClassChooserPropertyEditor(PropertiesEditingView<Composite> view, ElementEditor elementEditor, PropertyEditorViewer<SingleLinePropertyViewer> propertyEditorViewer) {
+		this.view = view;
+		this.elementEditor = elementEditor;
+		this.propertyEditorViewer = propertyEditorViewer;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see org.eclipse.emf.eef.runtime.ui.view.propertyeditors.PropertyEditor#init(org.eclipse.emf.ecore.EStructuralFeature)
+	 */
+	public void init(EStructuralFeature feature) {
+		this.feature = feature;
+		EEFViewerInput input = new EEFViewerInput(view.getEditingComponent().getEditingContext(), feature);
+		propertyEditorViewer.getViewer().setInput(input);
+		initListener();
+		GridData layoutData = new GridData(GridData.FILL_HORIZONTAL);
+		propertyEditorViewer.getViewer().getControl().setLayoutData(layoutData);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see org.eclipse.emf.eef.runtime.ui.view.propertyeditors.PropertyEditor#getPropertyEditorViewer()
+	 */
+	public PropertyEditorViewer<?> getPropertyEditorViewer() {
+		return propertyEditorViewer;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see org.eclipse.emf.eef.runtime.ui.view.propertyeditors.MonovaluedPropertyEditor#setValue(java.lang.Object)
+	 */
+	public void setValue(Object value) {
+		propertyEditorViewer.getViewer().refresh();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see org.eclipse.emf.eef.runtime.ui.view.propertyeditors.MonovaluedPropertyEditor#unsetValue()
+	 */
+	public void unsetValue() {
+		propertyEditorViewer.getViewer().setInput(null);		
+	}
+
+	/**
+	 * Initialize the listener on the MultiLinePropertyViewer.
+	 */
+	protected void initListener() {
+		if (listener == null) {
+			listener = new SingleLinePropertyViewerListener() {
+
+				/**
+				 * {@inheritDoc}
+				 * @see org.eclipse.emf.eef.runtime.ui.widgets.EComboEditor.EComboListener#set()
+				 */
+				public void set() {
+					IJavaElement[] elements = new IJavaElement[] { getContainingProject() };
+					IJavaSearchScope scope = SearchEngine.createJavaSearchScope(elements);
+					FilteredTypesSelectionDialog dialog = new FilteredTypesSelectionDialog(view.getContents().getShell(), false, PlatformUI.getWorkbench().getProgressService(), scope, IJavaSearchConstants.CLASS);
+					dialog.open();
+					Object result = dialog.getFirstResult();
+					if (result instanceof IType) {
+						IType javaClass = (IType)result;
+						firePropertiesChanged(view.getEditingComponent(), new PropertiesEditingEventImpl(view, elementEditor, PropertiesEditingEvent.SET, null, javaClass.getClass().getName()));
+					}
+				}
+
+				/**
+				 * {@inheritDoc}
+				 * @see org.eclipse.emf.eef.runtime.ui.widgets.EComboEditor.EComboListener#clear()
+				 */
+				public void clear() {
+					firePropertiesChanged(view.getEditingComponent(), new PropertiesEditingEventImpl(view, elementEditor, PropertiesEditingEvent.UNSET, null, null));
+					propertyEditorViewer.getViewer().refresh();
+				}
+				
+			};
+			propertyEditorViewer.getViewer().addSingleLinePropertyViewerListener(listener);
+		}
+	}
+
+
+	private IJavaProject getContainingProject() {
+		URI uri = view.getEditingComponent().getEObject().eResource().getURI();
+		if (uri.isPlatformResource()) {
+			String segment = uri.segment(1);
+			IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(segment);
+			JavaModelManager manager = JavaModelManager.getJavaModelManager();
+			IJavaProject javaProject = manager.getJavaModel().getJavaProject(project);
+			return javaProject;
+		} else {
+			//TODO: for the moment I can't handle this case.
+		}
+
+		return null;
+	}
+}
---
+++ plugins/org.eclipse.emf.eef.runtime.ui.jdt/src/org/eclipse/emf/eef/runtime/ui/jdt/jdttoolkit/JDTToolkit.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.emf.eef.runtime.ui.jdt.jdttoolkit;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.emf.eef.runtime.ui.jdt.internal.jdttoolkit.propertyeditors.JavaClassChooserPropertyEditorFactory;
+import org.eclipse.emf.eef.runtime.ui.swt.resources.ImageManager;
+import org.eclipse.emf.eef.runtime.ui.swt.viewer.EditUIProvidersFactory;
+import org.eclipse.emf.eef.runtime.ui.view.propertyeditors.impl.EEFToolkitImpl;
+import org.eclipse.emf.eef.views.toolkits.Toolkit;
+import org.eclipse.emf.eef.views.toolkits.ToolkitsFactory;
+
+/**
+ * @author <a href="mailto:goulwen.lefur@obeo.fr">Goulwen Le Fur</a>
+ *
+ */
+public class JDTToolkit extends EEFToolkitImpl<Composite> {
+
+	/**
+	 * EEF JDT Toolkit name.
+	 */
+	public static final String JDT_TOOLKIT_NAME = "jdt";
+	
+	private static final Toolkit toolkit = ToolkitsFactory.eINSTANCE.createToolkit();
+	
+	static {
+		toolkit.setName(JDT_TOOLKIT_NAME);
+	}
+
+	private EditUIProvidersFactory editUIProvidersFactory;
+
+	private ImageManager imageManager;
+	
+	
+	/**
+	 * 
+	 */
+	public JDTToolkit() {
+		addPropertyEditorFactory(new JavaClassChooserPropertyEditorFactory(this));
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see org.eclipse.emf.eef.runtime.ui.view.propertyeditors.ModelPropertyEditorFactory#getModel()
+	 */
+	public Toolkit getModel() {
+		return toolkit;
+	}
+
+	/**
+	 * @return the editUIProvidersFactory
+	 */
+	public EditUIProvidersFactory getEditUIProvidersFactory() {
+		return editUIProvidersFactory;
+	}
+
+	/**
+	 * @param editUIProvidersFactory the editUIProvidersFactory to set
+	 */
+	public void setEditUIProvidersFactory(EditUIProvidersFactory editUIProvidersFactory) {
+		this.editUIProvidersFactory = editUIProvidersFactory;
+	}
+
+	/**
+	 * @return the imageManager
+	 */
+	public ImageManager getImageManager() {
+		return imageManager;
+	}
+
+	/**
+	 * @param imageManager the imageManager to set
+	 */
+	public void setImageManager(ImageManager imageManager) {
+		this.imageManager = imageManager;
+	}
+
+}
