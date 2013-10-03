22106270f701df4e6d21b488241d201f182cbe4e
Goulwen Le Fur <goulwen.lefur@obeo.fr>
2013-10-03 10:44:32 +0200

    UPDATED: SingleCompositionPropertyEditor isn't a FilterablePropertyEditor for the moment.

--- plugins/org.eclipse.emf.eef.runtime.ui.swt.e3/src/org/eclipse/emf/eef/runtime/ui/swt/e3/internal/view/propertyeditors/impl/emfpropertiestoolkit/singlecontainmenteditor/SingleContainmentFormPropertyEditor.java
+++ plugins/org.eclipse.emf.eef.runtime.ui.swt.e3/src/org/eclipse/emf/eef/runtime/ui/swt/e3/internal/view/propertyeditors/impl/emfpropertiestoolkit/singlecontainmenteditor/SingleContainmentFormPropertyEditor.java
@@ -3,37 +3,29 @@
  */
 package org.eclipse.emf.eef.runtime.ui.swt.e3.internal.view.propertyeditors.impl.emfpropertiestoolkit.singlecontainmenteditor;
 
-import java.util.Collection;
-
 import org.eclipse.emf.eef.runtime.context.PropertiesEditingContext;
 import org.eclipse.emf.eef.runtime.ui.swt.e3.internal.view.propertyeditors.impl.StandardFormPropertyEditor;
 import org.eclipse.emf.eef.runtime.ui.swt.e3.internal.widgets.FormSingleLinePropertyViewer;
 import org.eclipse.emf.eef.runtime.ui.swt.internal.widgets.SingleLinePropertyViewer;
 import org.eclipse.emf.eef.runtime.ui.swt.resources.ImageManager;
-import org.eclipse.emf.eef.runtime.ui.swt.view.propertyeditors.FilterablePropertyEditor;
 import org.eclipse.emf.eef.runtime.ui.swt.viewer.EditUIProvidersFactory;
 import org.eclipse.emf.eef.runtime.ui.view.PropertiesEditingView;
 import org.eclipse.emf.eef.views.ElementEditor;
-import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 
-import com.google.common.collect.Lists;
-
 /**
  * @author <a href="mailto:goulwen.lefur@obeo.fr">Goulwen Le Fur</a>
  *
  */
-public class SingleContainmentFormPropertyEditor extends StandardFormPropertyEditor<SingleLinePropertyViewer> implements FilterablePropertyEditor {
+public class SingleContainmentFormPropertyEditor extends StandardFormPropertyEditor<SingleLinePropertyViewer> {
 
 	private EditUIProvidersFactory editUIProvidersFactory;
 	private ImageManager imageManager;
 	
 	private FormSingleLinePropertyViewer singleContainmentEditor;
-
-	private Collection<ViewerFilter> filters;
 
 	/**
 	 * @param editUIProvidersFactory
@@ -45,7 +37,6 @@ public class SingleContainmentFormPropertyEditor extends StandardFormPropertyEdi
 		super(view, elementEditor);
 		this.editUIProvidersFactory = editUIProvidersFactory;
 		this.imageManager = imageManager;
-		this.filters = Lists.newArrayList();
 	}
 
 	/**
@@ -69,30 +60,6 @@ public class SingleContainmentFormPropertyEditor extends StandardFormPropertyEdi
 		singleContainmentEditor.createContents();
 		GridData layoutData = new GridData(GridData.FILL_HORIZONTAL);
 		singleContainmentEditor.getControl().setLayoutData(layoutData);
-	}
-
-	/**
-	 * {@inheritDoc}
-	 * @see org.eclipse.emf.eef.runtime.ui.swt.view.propertyeditors.FilterablePropertyEditor#addFilter(org.eclipse.jface.viewers.ViewerFilter)
-	 */
-	public void addFilter(ViewerFilter filter) {
-		filters.add(filter);
-	}
-
-	/**
-	 * {@inheritDoc}
-	 * @see org.eclipse.emf.eef.runtime.ui.swt.view.propertyeditors.FilterablePropertyEditor#removeFilter(org.eclipse.jface.viewers.ViewerFilter)
-	 */
-	public void removeFilter(ViewerFilter filter) {
-		filters.remove(filter);
-	}
-
-	/**
-	 * {@inheritDoc}
-	 * @see org.eclipse.emf.eef.runtime.ui.swt.view.propertyeditors.FilterablePropertyEditor#getFilters()
-	 */
-	public Collection<ViewerFilter> getFilters() {
-		return filters;
 	}
 
 	/**
