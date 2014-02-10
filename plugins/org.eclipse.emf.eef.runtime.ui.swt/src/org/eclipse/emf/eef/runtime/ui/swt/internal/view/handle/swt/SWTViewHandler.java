7bca945c3c37c2bc8811e16d3fadf4903560da67
Nathalie Lepine <nathalie.lepine@obeo.fr>
2014-02-10 12:13:14 +0100

    fix view handler

--- plugins/org.eclipse.emf.eef.runtime.ui.swt/src/org/eclipse/emf/eef/runtime/ui/swt/internal/view/handle/swt/SWTViewHandler.java
+++ plugins/org.eclipse.emf.eef.runtime.ui.swt/src/org/eclipse/emf/eef/runtime/ui/swt/internal/view/handle/swt/SWTViewHandler.java
@@ -13,9 +13,7 @@ package org.eclipse.emf.eef.runtime.ui.swt.internal.view.handle.swt;
 import org.eclipse.emf.eef.runtime.binding.PropertiesEditingComponent;
 import org.eclipse.emf.eef.runtime.editingModel.JavaView;
 import org.eclipse.emf.eef.runtime.editingModel.View;
-import org.eclipse.emf.eef.runtime.logging.EEFLogger;
 import org.eclipse.emf.eef.runtime.ui.internal.view.handle.reflect.ReflectViewHandler;
-import org.eclipse.emf.eef.runtime.view.lock.EEFLockManagerProvider;
 import org.eclipse.swt.widgets.Composite;
 
 /**
@@ -23,25 +21,6 @@ import org.eclipse.swt.widgets.Composite;
  * 
  */
 public class SWTViewHandler extends ReflectViewHandler<Composite> {
-
-	private EEFLockManagerProvider lockManagerProvider;
-	private EEFLogger logger;
-
-	/**
-	 * @param lockManagerProvider
-	 *            the lockManagerProvider to set
-	 */
-	public void setLockManagerProvider(EEFLockManagerProvider lockManagerProvider) {
-		this.lockManagerProvider = lockManagerProvider;
-	}
-
-	/**
-	 * @param logger
-	 *            the logger to set
-	 */
-	public void setLogger(EEFLogger logger) {
-		this.logger = logger;
-	}
 
 	/**
 	 * {@inheritDoc}
@@ -64,15 +43,6 @@ public class SWTViewHandler extends ReflectViewHandler<Composite> {
 		} else {
 			return false;
 		}
-	}
-
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see org.eclipse.emf.eef.runtime.view.handle.ViewHandler#getLogger()
-	 */
-	public EEFLogger getLogger() {
-		return logger;
 	}
 
 	/**
