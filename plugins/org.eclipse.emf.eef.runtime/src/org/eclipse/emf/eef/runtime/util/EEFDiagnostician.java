67a503ead2b9df1e442eec7f0994f0f65ba2d608
Nathalie Lepine <nathalie.lepine@obeo.fr>
2015-03-06 16:31:54 +0100

    fix copirignt

--- plugins/org.eclipse.emf.eef.runtime/src/org/eclipse/emf/eef/runtime/util/EEFDiagnostician.java
+++ plugins/org.eclipse.emf.eef.runtime/src/org/eclipse/emf/eef/runtime/util/EEFDiagnostician.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.emf.eef.runtime.util;
 
 import java.util.Map;
@@ -8,6 +18,12 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.Diagnostician;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 
+/**
+ * Diagnostician with correct EObject label in messages.
+ * 
+ * @author <a href="mailto:nathalie.lepine@obeo.fr">Nathalie Lepine</a>
+ *
+ */
 public class EEFDiagnostician extends Diagnostician {
 	private AdapterFactory adapterFactory;
 
