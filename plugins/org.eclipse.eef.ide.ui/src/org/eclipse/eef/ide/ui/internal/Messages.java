eclipse-eef/org.eclipse.eef

Custom widgets support.
Change-Id: I311a831f071ba9c85594c68c21836a584a8a55e6
Signed-off-by: Mélanie Bats <melanie.bats@obeo.fr>

plugins/org.eclipse.eef.ide.ui/src/org/eclipse/eef/ide/ui/internal/Messages.java
@@ -31,6 +31,12 @@ public final class Messages {
 	@TranslatableMessage
 	public static String EEFPreferencePage_DebugButton_Tooltip;
 
+	@TranslatableMessage
+	public static String DescriptorRegistryEventListener_missingAttribute;
+
+	@TranslatableMessage
+	public static String DescriptorRegistryEventListener_extensionAlreadyContributed;
+
 	// CHECKSTYLE:ON
 
 	/**

isabella232/eef

plugins/org.eclipse.eef.ide.ui/src/org/eclipse/eef/ide/ui/internal/Messages.java
@@ -31,6 +31,12 @@ public final class Messages {
 	@TranslatableMessage
 	public static String EEFPreferencePage_DebugButton_Tooltip;
 
+	@TranslatableMessage
+	public static String EEFIdeUiPlugin_lifecycleManagerNotFound;
+
+	@TranslatableMessage
+	public static String EEFIdeUiPlugin_lifecycleManagerInvalid;
+
 	// CHECKSTYLE:ON
 
 	/**