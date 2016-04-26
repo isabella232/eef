eclipse-eef/org.eclipse.eef

Support reference widget.
One widget implements containment or not, single/multiple valued
references.

Change-Id: I4cb9c72106ae8003508bbaba362263d6e687c9a5
Signed-off-by: Mélanie Bats <melanie.bats@obeo.fr>

plugins/org.eclipse.eef.core/src/org/eclipse/eef/core/api/controllers/EEFControllersFactory.java
@@ -16,6 +16,7 @@ import org.eclipse.eef.EEFGroupDescription;
 import org.eclipse.eef.EEFLabelDescription;
 import org.eclipse.eef.EEFPageDescription;
 import org.eclipse.eef.EEFRadioDescription;
+import org.eclipse.eef.EEFReferenceDescription;
 import org.eclipse.eef.EEFSelectDescription;
 import org.eclipse.eef.EEFTextDescription;
 import org.eclipse.eef.core.internal.controllers.EEFButtonController;
@@ -23,6 +24,7 @@ import org.eclipse.eef.core.internal.controllers.EEFCheckboxController;
 import org.eclipse.eef.core.internal.controllers.EEFGroupController;
 import org.eclipse.eef.core.internal.controllers.EEFLabelController;
 import org.eclipse.eef.core.internal.controllers.EEFRadioController;
+import org.eclipse.eef.core.internal.controllers.EEFReferenceController;
 import org.eclipse.eef.core.internal.controllers.EEFSectionController;
 import org.eclipse.eef.core.internal.controllers.EEFSelectController;
 import org.eclipse.eef.core.internal.controllers.EEFTextController;
@@ -171,5 +173,23 @@ public class EEFControllersFactory {
 	 */
 	public IEEFSectionController createSectionController(EEFPageDescription description, IVariableManager variableManager, IInterpreter interpreter) {
 		return new EEFSectionController(variableManager, interpreter, description);
+	}
+
+	/**
+	 * Creates a reference controller.
+	 *
+	 * @param description
+	 *            The description
+	 * @param variableManager
+	 *            The variable manager
+	 * @param interpreter
+	 *            The interpreter
+	 * @param editingDomain
+	 *            The editing domain
+	 * @return The reference controller.
+	 */
+	public IEEFReferenceController createReferenceController(EEFReferenceDescription description, IVariableManager variableManager,
+			IInterpreter interpreter, TransactionalEditingDomain editingDomain) {
+		return new EEFReferenceController(variableManager, interpreter, description, editingDomain);
 	}
 }

isabella232/eef

plugins/org.eclipse.eef.core/src/org/eclipse/eef/core/api/controllers/EEFControllersFactory.java
@@ -16,6 +16,7 @@ import org.eclipse.eef.EEFGroupDescription;
 import org.eclipse.eef.EEFLabelDescription;
 import org.eclipse.eef.EEFPageDescription;
 import org.eclipse.eef.EEFRadioDescription;
+import org.eclipse.eef.EEFReferenceDescription;
 import org.eclipse.eef.EEFSelectDescription;
 import org.eclipse.eef.EEFTextDescription;
 import org.eclipse.eef.core.api.EditingContextAdapter;
@@ -24,6 +25,7 @@ import org.eclipse.eef.core.internal.controllers.EEFCheckboxController;
 import org.eclipse.eef.core.internal.controllers.EEFGroupController;
 import org.eclipse.eef.core.internal.controllers.EEFLabelController;
 import org.eclipse.eef.core.internal.controllers.EEFRadioController;
+import org.eclipse.eef.core.internal.controllers.EEFReferenceController;
 import org.eclipse.eef.core.internal.controllers.EEFSectionController;
 import org.eclipse.eef.core.internal.controllers.EEFSelectController;
 import org.eclipse.eef.core.internal.controllers.EEFTextController;
@@ -171,5 +173,23 @@ public class EEFControllersFactory {
 	 */
 	public IEEFSectionController createSectionController(EEFPageDescription description, IVariableManager variableManager, IInterpreter interpreter) {
 		return new EEFSectionController(variableManager, interpreter, description);
+	}
+
+	/**
+	 * Creates a reference controller.
+	 *
+	 * @param description
+	 *            The description
+	 * @param variableManager
+	 *            The variable manager
+	 * @param interpreter
+	 *            The interpreter
+	 * @param contextAdapter
+	 *            The editing context adapter
+	 * @return The reference controller.
+	 */
+	public IEEFReferenceController createReferenceController(EEFReferenceDescription description, IVariableManager variableManager,
+			IInterpreter interpreter, EditingContextAdapter contextAdapter) {
+		return new EEFReferenceController(variableManager, interpreter, description, contextAdapter);
 	}
 }