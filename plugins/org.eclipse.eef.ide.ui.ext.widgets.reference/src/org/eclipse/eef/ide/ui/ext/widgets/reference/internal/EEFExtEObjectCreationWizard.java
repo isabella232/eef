eclipse-eef/org.eclipse.eef

[496057] Add support for advanced custom widgets
1) Add a new reference widget configured using the name of an EReference
2) Add the method getEditingDomain() on EditingContextAdapter
3) Fix a potential NPE with the lifecycle manager providers

Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=496057
Change-Id: Ib6597e1df8caa345c5e89fe44a6b40eee30d63e2
Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

samples/org.eclipse.eef.ide.ui.ext.widgets.reference/src/org/eclipse/eef/ide/ui/ext/widgets/reference/internal/EEFExtEObjectCreationWizard.java
@@ -1,0 +1,121 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.eef.ide.ui.ext.widgets.reference.internal;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.eef.core.api.EditingContextAdapter;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.wizard.Wizard;
+
+/**
+ * The wizard used to create a new value for the reference.
+ *
+ * @author sbegaudeau
+ */
+public class EEFExtEObjectCreationWizard extends Wizard {
+
+	/**
+	 * The current EObject.
+	 */
+	private EObject currentEObject;
+
+	/**
+	 * The EReference.
+	 */
+	private EReference eReference;
+
+	/**
+	 * The editing context adapter.
+	 */
+	private EditingContextAdapter editingContextAdapter;
+
+	/**
+	 * The page used to create the new EObject.
+	 */
+	private EEFExtEObjectCreationPage eObjectCreationPage;
+
+	/**
+	 * The constructor.
+	 *
+	 * @param currentEObject
+	 *            The current EObject
+	 * @param eReference
+	 *            The EReference
+	 * @param editingContextAdapter
+	 *            The editing context adapter
+	 */
+	public EEFExtEObjectCreationWizard(EObject currentEObject, EReference eReference, EditingContextAdapter editingContextAdapter) {
+		this.currentEObject = currentEObject;
+		this.eReference = eReference;
+		this.editingContextAdapter = editingContextAdapter;
+		this.setWindowTitle(Messages.ReferenceCreationWizard_windowTitle);
+		ImageDescriptor imageDescriptor = ExtendedImageRegistry.INSTANCE.getImageDescriptor(EEFExtReferenceUIPlugin.getPlugin().getImage(
+				EEFExtReferenceUIPlugin.Implementation.NEW_WIZBAN_PATH));
+		this.setDefaultPageImageDescriptor(imageDescriptor);
+		this.setNeedsProgressMonitor(true);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.jface.wizard.Wizard#addPages()
+	 */
+	@Override
+	public void addPages() {
+		super.addPages();
+
+		this.eObjectCreationPage = new EEFExtEObjectCreationPage(this.currentEObject, this.eReference, this.editingContextAdapter);
+		this.addPage(this.eObjectCreationPage);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
+	 */
+	@Override
+	public boolean performFinish() {
+		boolean finishedProperly = true;
+
+		IRunnableWithProgress runnableWithProgress = new IRunnableWithProgress() {
+			@Override
+			public void run(final IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+				Runnable runnable = new Runnable() {
+					@Override
+					public void run() {
+						EEFExtEObjectCreationWizard.this.eObjectCreationPage.performFinish(monitor);
+					}
+				};
+
+				EEFExtEObjectCreationWizard.this.editingContextAdapter.performModelChange(runnable);
+			}
+		};
+
+		try {
+			this.getContainer().run(false, false, runnableWithProgress);
+		} catch (InvocationTargetException | InterruptedException e) {
+			finishedProperly = false;
+			IStatus status = new Status(IStatus.ERROR, EEFExtReferenceUIPlugin.PLUGIN_ID, e.getMessage());
+			EEFExtReferenceUIPlugin.getPlugin().getLog().log(status);
+		}
+
+		return finishedProperly;
+	}
+
+}

samples/org.eclipse.eef.ide.ui.ext.widgets.reference/src/org/eclipse/eef/ide/ui/ext/widgets/reference/internal/EEFExtEObjectSelectionWizard.java
@@ -1,0 +1,121 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.eef.ide.ui.ext.widgets.reference.internal;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.eef.core.api.EditingContextAdapter;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.wizard.Wizard;
+
+/**
+ * The wizard used to select a value for the reference.
+ *
+ * @author sbegaudeau
+ */
+public class EEFExtEObjectSelectionWizard extends Wizard {
+
+	/**
+	 * The current EObject.
+	 */
+	private EObject currentEObject;
+
+	/**
+	 * The EReference.
+	 */
+	private EReference eReference;
+
+	/**
+	 * The editing context adapter.
+	 */
+	private EditingContextAdapter editingContextAdapter;
+
+	/**
+	 * The page used to select a new EObject.
+	 */
+	private EEFExtEObjectSelectionPage eObjectSelectionPage;
+
+	/**
+	 * The constructor.
+	 *
+	 * @param currentEObject
+	 *            The current EObject
+	 * @param eReference
+	 *            The EReference
+	 * @param editingContextAdapter
+	 *            The editing context adapter
+	 */
+	public EEFExtEObjectSelectionWizard(EObject currentEObject, EReference eReference, EditingContextAdapter editingContextAdapter) {
+		this.currentEObject = currentEObject;
+		this.eReference = eReference;
+		this.editingContextAdapter = editingContextAdapter;
+		this.setWindowTitle(Messages.ReferenceSelectionWizard_windowTitle);
+		ImageDescriptor imageDescriptor = ExtendedImageRegistry.INSTANCE.getImageDescriptor(EEFExtReferenceUIPlugin.getPlugin().getImage(
+				EEFExtReferenceUIPlugin.Implementation.NEW_WIZBAN_PATH));
+		this.setDefaultPageImageDescriptor(imageDescriptor);
+		this.setNeedsProgressMonitor(true);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.jface.wizard.Wizard#addPages()
+	 */
+	@Override
+	public void addPages() {
+		super.addPages();
+
+		this.eObjectSelectionPage = new EEFExtEObjectSelectionPage(this.currentEObject, this.eReference, this.editingContextAdapter);
+		this.addPage(this.eObjectSelectionPage);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
+	 */
+	@Override
+	public boolean performFinish() {
+		boolean finishedProperly = true;
+
+		IRunnableWithProgress runnableWithProgress = new IRunnableWithProgress() {
+			@Override
+			public void run(final IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+				Runnable runnable = new Runnable() {
+					@Override
+					public void run() {
+						EEFExtEObjectSelectionWizard.this.eObjectSelectionPage.performFinish(monitor);
+					}
+				};
+
+				EEFExtEObjectSelectionWizard.this.editingContextAdapter.performModelChange(runnable);
+			}
+		};
+
+		try {
+			this.getContainer().run(false, false, runnableWithProgress);
+		} catch (InvocationTargetException | InterruptedException e) {
+			finishedProperly = false;
+			IStatus status = new Status(IStatus.ERROR, EEFExtReferenceUIPlugin.PLUGIN_ID, e.getMessage());
+			EEFExtReferenceUIPlugin.getPlugin().getLog().log(status);
+		}
+
+		return finishedProperly;
+	}
+
+}

samples/org.eclipse.eef.ide.ui.ext.widgets.reference/src/org/eclipse/eef/ide/ui/ext/widgets/reference/internal/Messages.java
@@ -1,0 +1,92 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.eef.ide.ui.ext.widgets.reference.internal;
+
+import org.eclipse.eef.common.api.utils.I18N;
+import org.eclipse.eef.common.api.utils.I18N.TranslatableMessage;
+
+/**
+ * Utility class used for the internationalization.
+ *
+ * @author sbegaudeau
+ */
+public final class Messages {
+
+	static {
+		I18N.initializeMessages(Messages.class, EEFExtReferenceUIPlugin.INSTANCE);
+	}
+
+	// @CHECKSTYLE:OFF
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizard_windowTitle;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_title;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_description;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_eClassToCreateLabel;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_eContainerSelectionLabel;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_eContainerToUseLabel;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_missingEClassToCreate;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_missingEContainer;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_missingContainmentEReference;
+
+	@TranslatableMessage
+	public static String ReferenceSelectionWizard_windowTitle;
+
+	@TranslatableMessage
+	public static String ReferenceSelectionWizardPage_title;
+
+	@TranslatableMessage
+	public static String ReferenceSelectionWizardPage_description;
+
+	@TranslatableMessage
+	public static String ReferenceSelectionWizardPage_missingEObject;
+
+	@TranslatableMessage
+	public static String SingleReference_noValue;
+
+	@TranslatableMessage
+	public static String ReferenceBrowseButton_tooltipText;
+
+	@TranslatableMessage
+	public static String ReferenceAddButton_tooltipText;
+
+	@TranslatableMessage
+	public static String ReferenceRemoveButton_tooltipText;
+
+	@TranslatableMessage
+	public static String ReferenceUpButton_tooltipText;
+
+	@TranslatableMessage
+	public static String ReferenceDownButton_tooltipText;
+
+	/**
+	 * The constructor.
+	 */
+	private Messages() {
+		// Prevent instantiation
+	}
+}

isabella232/eef

plugins/org.eclipse.eef.ide.ui.ext.widgets.reference/src/org/eclipse/eef/ide/ui/ext/widgets/reference/internal/EEFExtEObjectCreationWizard.java
@@ -1,0 +1,121 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.eef.ide.ui.ext.widgets.reference.internal;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.eef.core.api.EditingContextAdapter;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.wizard.Wizard;
+
+/**
+ * The wizard used to create a new value for the reference.
+ *
+ * @author sbegaudeau
+ */
+public class EEFExtEObjectCreationWizard extends Wizard {
+
+	/**
+	 * The target.
+	 */
+	private EObject target;
+
+	/**
+	 * The EReference.
+	 */
+	private EReference eReference;
+
+	/**
+	 * The editing context adapter.
+	 */
+	private EditingContextAdapter editingContextAdapter;
+
+	/**
+	 * The page used to create the new EObject.
+	 */
+	private EEFExtEObjectCreationPage eObjectCreationPage;
+
+	/**
+	 * The constructor.
+	 *
+	 * @param target
+	 *            The target
+	 * @param eReference
+	 *            The EReference
+	 * @param editingContextAdapter
+	 *            The editing context adapter
+	 */
+	public EEFExtEObjectCreationWizard(EObject target, EReference eReference, EditingContextAdapter editingContextAdapter) {
+		this.target = target;
+		this.eReference = eReference;
+		this.editingContextAdapter = editingContextAdapter;
+		this.setWindowTitle(Messages.ReferenceCreationWizard_windowTitle);
+		ImageDescriptor imageDescriptor = ExtendedImageRegistry.INSTANCE.getImageDescriptor(EEFExtReferenceUIPlugin.getPlugin().getImage(
+				EEFExtReferenceUIPlugin.Implementation.NEW_WIZBAN_PATH));
+		this.setDefaultPageImageDescriptor(imageDescriptor);
+		this.setNeedsProgressMonitor(true);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.jface.wizard.Wizard#addPages()
+	 */
+	@Override
+	public void addPages() {
+		super.addPages();
+
+		this.eObjectCreationPage = new EEFExtEObjectCreationPage(this.target, this.eReference, this.editingContextAdapter);
+		this.addPage(this.eObjectCreationPage);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
+	 */
+	@Override
+	public boolean performFinish() {
+		boolean finishedProperly = true;
+
+		IRunnableWithProgress runnableWithProgress = new IRunnableWithProgress() {
+			@Override
+			public void run(final IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+				Runnable runnable = new Runnable() {
+					@Override
+					public void run() {
+						EEFExtEObjectCreationWizard.this.eObjectCreationPage.performFinish(monitor);
+					}
+				};
+
+				EEFExtEObjectCreationWizard.this.editingContextAdapter.performModelChange(runnable);
+			}
+		};
+
+		try {
+			this.getContainer().run(false, false, runnableWithProgress);
+		} catch (InvocationTargetException | InterruptedException e) {
+			finishedProperly = false;
+			IStatus status = new Status(IStatus.ERROR, EEFExtReferenceUIPlugin.PLUGIN_ID, e.getMessage());
+			EEFExtReferenceUIPlugin.getPlugin().getLog().log(status);
+		}
+
+		return finishedProperly;
+	}
+
+}

plugins/org.eclipse.eef.ide.ui.ext.widgets.reference/src/org/eclipse/eef/ide/ui/ext/widgets/reference/internal/EEFExtEObjectSelectionWizard.java
@@ -1,0 +1,121 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.eef.ide.ui.ext.widgets.reference.internal;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.eef.core.api.EditingContextAdapter;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.wizard.Wizard;
+
+/**
+ * The wizard used to select a value for the reference.
+ *
+ * @author sbegaudeau
+ */
+public class EEFExtEObjectSelectionWizard extends Wizard {
+
+	/**
+	 * The target.
+	 */
+	private EObject target;
+
+	/**
+	 * The EReference.
+	 */
+	private EReference eReference;
+
+	/**
+	 * The editing context adapter.
+	 */
+	private EditingContextAdapter editingContextAdapter;
+
+	/**
+	 * The page used to select a new EObject.
+	 */
+	private EEFExtEObjectSelectionPage eObjectSelectionPage;
+
+	/**
+	 * The constructor.
+	 *
+	 * @param target
+	 *            The target
+	 * @param eReference
+	 *            The EReference
+	 * @param editingContextAdapter
+	 *            The editing context adapter
+	 */
+	public EEFExtEObjectSelectionWizard(EObject target, EReference eReference, EditingContextAdapter editingContextAdapter) {
+		this.target = target;
+		this.eReference = eReference;
+		this.editingContextAdapter = editingContextAdapter;
+		this.setWindowTitle(Messages.ReferenceSelectionWizard_windowTitle);
+		ImageDescriptor imageDescriptor = ExtendedImageRegistry.INSTANCE.getImageDescriptor(EEFExtReferenceUIPlugin.getPlugin().getImage(
+				EEFExtReferenceUIPlugin.Implementation.NEW_WIZBAN_PATH));
+		this.setDefaultPageImageDescriptor(imageDescriptor);
+		this.setNeedsProgressMonitor(true);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.jface.wizard.Wizard#addPages()
+	 */
+	@Override
+	public void addPages() {
+		super.addPages();
+
+		this.eObjectSelectionPage = new EEFExtEObjectSelectionPage(this.target, this.eReference, this.editingContextAdapter);
+		this.addPage(this.eObjectSelectionPage);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
+	 */
+	@Override
+	public boolean performFinish() {
+		boolean finishedProperly = true;
+
+		IRunnableWithProgress runnableWithProgress = new IRunnableWithProgress() {
+			@Override
+			public void run(final IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+				Runnable runnable = new Runnable() {
+					@Override
+					public void run() {
+						EEFExtEObjectSelectionWizard.this.eObjectSelectionPage.performFinish(monitor);
+					}
+				};
+
+				EEFExtEObjectSelectionWizard.this.editingContextAdapter.performModelChange(runnable);
+			}
+		};
+
+		try {
+			this.getContainer().run(false, false, runnableWithProgress);
+		} catch (InvocationTargetException | InterruptedException e) {
+			finishedProperly = false;
+			IStatus status = new Status(IStatus.ERROR, EEFExtReferenceUIPlugin.PLUGIN_ID, e.getMessage());
+			EEFExtReferenceUIPlugin.getPlugin().getLog().log(status);
+		}
+
+		return finishedProperly;
+	}
+
+}

plugins/org.eclipse.eef.ide.ui.ext.widgets.reference/src/org/eclipse/eef/ide/ui/ext/widgets/reference/internal/Messages.java
@@ -1,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.eef.ide.ui.ext.widgets.reference.internal;
+
+import org.eclipse.eef.common.api.utils.I18N;
+import org.eclipse.eef.common.api.utils.I18N.TranslatableMessage;
+
+/**
+ * Utility class used for the internationalization.
+ *
+ * @author sbegaudeau
+ */
+public final class Messages {
+
+	static {
+		I18N.initializeMessages(Messages.class, EEFExtReferenceUIPlugin.INSTANCE);
+	}
+
+	// @CHECKSTYLE:OFF
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizard_windowTitle;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_title;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_description;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_eClassToCreateLabel;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_eContainerSelectionLabel;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_eContainerToUseLabel;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_missingEClassToCreate;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_missingEContainer;
+
+	@TranslatableMessage
+	public static String ReferenceCreationWizardPage_missingContainmentEReference;
+
+	@TranslatableMessage
+	public static String ReferenceSelectionWizard_windowTitle;
+
+	@TranslatableMessage
+	public static String ReferenceSelectionWizardPage_title;
+
+	@TranslatableMessage
+	public static String ReferenceSelectionWizardPage_description;
+
+	@TranslatableMessage
+	public static String ReferenceSelectionWizardPage_missingEObject;
+
+	@TranslatableMessage
+	public static String SingleReference_noValue;
+
+	@TranslatableMessage
+	public static String ReferenceBrowseButton_tooltipText;
+
+	@TranslatableMessage
+	public static String ReferenceAddButton_tooltipText;
+
+	@TranslatableMessage
+	public static String ReferenceRemoveButton_containmentTooltipText;
+
+	@TranslatableMessage
+	public static String ReferenceRemoveButton_nonContainmentTooltipText;
+
+	@TranslatableMessage
+	public static String ReferenceUpButton_tooltipText;
+
+	@TranslatableMessage
+	public static String ReferenceDownButton_tooltipText;
+
+	/**
+	 * The constructor.
+	 */
+	private Messages() {
+		// Prevent instantiation
+	}
+}