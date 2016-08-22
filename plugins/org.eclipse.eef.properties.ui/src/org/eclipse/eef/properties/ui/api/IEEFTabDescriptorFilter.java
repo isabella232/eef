eclipse-eef/org.eclipse.eef

[489294] Provide a tab descriptor filter
This patch provides a new extension point to allow the users to filter
tabs according to a condition.

Bug: 489294
Change-Id: I362aaed008dbb67a7b3c2788ca87118b7f4f3bb7
Signed-off-by: Mélanie Bats <melanie.bats@obeo.fr>

plugins/org.eclipse.eef.properties.ui/src/org/eclipse/eef/properties/ui/api/IEEFTabDescriptorFilter.java
@@ -1,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.eef.properties.ui.api;
+
+/**
+ * This interface is used to filter {@link IEEFTabDescriptor}.
+ *
+ * @author mbats
+ */
+public interface IEEFTabDescriptorFilter {
+	/**
+	 * Returns if a tab descriptor must be filtered or not.
+	 *
+	 * @param tabDescriptor
+	 *            The tab descriptor
+	 *
+	 * @return True if the tab descriptor is visible otherwise false
+	 */
+	boolean isVisible(IEEFTabDescriptor tabDescriptor);
+}

isabella232/eef

plugins/org.eclipse.eef.properties.ui/src/org/eclipse/eef/properties/ui/api/IEEFTabDescriptorFilter.java
@@ -1,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.eef.properties.ui.api;
+
+/**
+ * This interface is used to filter {@link IEEFTabDescriptor}.
+ *
+ * @author mbats
+ */
+public interface IEEFTabDescriptorFilter {
+	/**
+	 * Returns if a tab descriptor must be filtered or not.
+	 *
+	 * @param tabDescriptor
+	 *            The tab descriptor
+	 *
+	 * @return <code>true</code> if the tab descriptor should be used, <code>false</code> otherwise
+	 */
+	boolean filter(IEEFTabDescriptor tabDescriptor);
+}