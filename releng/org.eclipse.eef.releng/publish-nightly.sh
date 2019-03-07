96264a2099e8b3d94cc6aff84d9b9ae29c651f20
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2019-03-07 16:21:30 +0100

    [releng] Fix an issue in the publish nightly script
    Change-Id: Ib9d31a97e10af9c441442f520e58f37afb249e11
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

--- releng/org.eclipse.eef.releng/publish-nightly.sh
+++ releng/org.eclipse.eef.releng/publish-nightly.sh
@@ -31,7 +31,7 @@ set -e
 
 # The full version (should be taken as an argument)
 export VERSION="2.1.2"
-export PLATFORM="2018-19"
+export PLATFORM="2018-12"
 
 # The type of build being published
 export BUILD_TYPE="nightly"
